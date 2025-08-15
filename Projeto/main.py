e919ac6e27af60511fcc1247d13ace01b71969ff
Heraldo Cruz <hccruz@gmail.com>
2025-08-14 23:54:31 -0300

    feat: Alterado diretório

---
+++ Projeto/main.py
@@ -0,0 +1,19 @@
+print('Jornada de Dados')
+
+# Variáveis
+idade = 53
+nome = 'Heraldo'
+
+# Print
+print(f'Nome: {nome}, Idade: {idade}')
+
+# Soma
+nova_idade = idade + 10
+print(f'Nova Idade: {nova_idade}')
+
+# Lista
+lista = [1, 2, 3, 4, 5]
+
+# Métodos
+lista_invertida = lista.reverse()
+print(f'Lista Invertida: {lista}')
